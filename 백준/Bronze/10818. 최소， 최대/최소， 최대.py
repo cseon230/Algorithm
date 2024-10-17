5dc00cd3a67fb29e800ea31f7891a48670e8fa1d
Seonha Choi <99326153+cseon230@users.noreply.github.com>
2024-10-17 20:05:07 +0900

    [Bronze III] Title: 최소, 최대, Time: 372 ms, Memory: 153840 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,4 +1,10 @@
 N = int(input())
 arr = list(map(int, input().split()))
-arr.sort()
-print(arr[0], arr[N-1])
+min = arr[0]
+max = arr[0]
+for num in arr[1:]:
+    if min > num:
+        min = num
+    if max < num:
+        max = num
+print(min, max)

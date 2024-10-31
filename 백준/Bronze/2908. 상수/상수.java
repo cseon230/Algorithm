08d8a955492951676aaf7d418532ab549a1624da
seonha-choi <99326153+cseon230@users.noreply.github.com>
2024-10-31 09:31:50 +0900

    [Bronze II] Title: 상수, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -6,21 +6,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String A = br.readLine();
-        StringBuilder sb = new StringBuilder(A); // StringBuilder는 reverse() 메서드를 제공한다.
-        String[] strArr = sb.reverse().toString().split(" ");
 
-        int[] intArr = new int[strArr.length];
+        String[] numbers = br.readLine().split(" "); // 한 줄 문자열을 공백으로 구분해서 리스트로 담기
+        int A = Integer.parseInt(new StringBuilder(numbers[0]).reverse().toString()); // StringBuilder는 .reverse() 메서드를 제공함
+        int B = Integer.parseInt(new StringBuilder(numbers[1]).reverse().toString());
 
-        for (int i = 0; i < intArr.length; i++) {
-            intArr[i] = Integer.parseInt(strArr[i]);
-        }
-
-        if (intArr[0] > intArr[1]) {
-            bw.write(String.valueOf(intArr[0]));
-        } else {
-            bw.write(String.valueOf(intArr[1]));
-        }
+        bw.write(String.valueOf(Math.max(A, B))); // 더 큰 수를 찾아서 출력하는 Math.max( ) 메서드 사용
+        // Math.max() 메서드는 두 개의 값 비교만 지원하므로 2개 이상의 값을 비교하려면 Math.max() 메서드를 중첩해서 사용해야 한다.
 
         bw.close();
         br.close();

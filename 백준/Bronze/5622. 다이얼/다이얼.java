916e3159443b060490d74867191a5b1a5d2e25b9
seonha-choi <99326153+cseon230@users.noreply.github.com>
2024-11-01 17:00:26 +0900

    [Bronze II] Title: 다이얼, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,49 +6,25 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String str = br.readLine();
-
-        Map<Integer, String> M = new HashMap<>();
-
-//        M.put("", 1);
-//        M.put("ABC", 2);
-//        M.put("DEF", 3);
-//        M.put("GHI", 4);
-//        M.put("JKL", 5);
-//        M.put("MNO", 6);
-//        M.put("PQRS", 7);
-//        M.put("TUV", 8);
-//        M.put("WXYZ", 9);
-
-        M.put(1, "");
-        M.put(2, "ABC");
-        M.put(3, "DEF");
-        M.put(4, "GHI");
-        M.put(5, "JKL");
-        M.put(6, "MNO");
-        M.put(7, "PQRS");
-        M.put(8, "TUV");
-        M.put(9, "WXYZ");
-
         int num = 0;
 
-        // WA가 들어온다고 가정
         for (int i = 0; i < str.length(); i++) {
-            for (int j = 1; j <= M.size(); j++) {
-                String ch = String.valueOf(str.charAt(i)); // W
-                String keyValue = M.get(j);
-                for (int x = 0; x < keyValue.length(); x++) {
-                    if (String.valueOf(keyValue.charAt(x)).equals(ch)) {
-                        num += j+1;
-                    }
-                }
-            }
+            char ch = str.charAt(i);
+
+            // 각 알파벳을 다이얼 숫자로 매핑
+            if (ch >= 'A' && ch <= 'C') num += 3;
+            else if (ch >= 'D' && ch <= 'F') num += 4;
+            else if (ch >= 'G' && ch <= 'I') num += 5;
+            else if (ch >= 'J' && ch <= 'L') num += 6;
+            else if (ch >= 'M' && ch <= 'O') num += 7;
+            else if (ch >= 'P' && ch <= 'S') num += 8;
+            else if (ch >= 'T' && ch <= 'V') num += 9;
+            else if (ch >= 'W' && ch <= 'Z') num += 10;
         }
 
         bw.write(String.valueOf(num));
-
-
-
-        bw.close();
+        bw.flush();
         br.close();
+        bw.close();
     }
 }
